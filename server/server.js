2d63c5a8622ea2c17509d43b000b0699d02fb99e
chrisyandev <czyanhome@hotmail.com>
2023-06-01 17:16:49 -0700

    add middleware for logging and error handling

--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
-require("dotenv").config();
+// imports
 const express = require("express");
+require("express-async-errors");
+require("dotenv").config();
+const noRouteHandler = require("./middleware/no-route-handler");
+const errorHandler = require("./middleware/error-handler");
+const morgan = require("morgan");
+
+// express
 const app = express();
 const port = process.env.PORT || 5000;
+
+//database
 const connectDB = require("./db/connect");
+
+// middleware
+app.use(morgan("tiny"));
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("e-commerce api");
+});
+
+// error middleware
+app.use(noRouteHandler);
+app.use(errorHandler);
 
 const start = async () => {
   try {
