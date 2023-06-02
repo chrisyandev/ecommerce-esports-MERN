ae7466e1b6b720a8acce0b17331e3e362fe621e8
chrisyandev <czyanhome@hotmail.com>
2023-06-01 19:19:50 -0700

    add bunch of Errors and throw error if email already exists

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,15 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const emailAlreadyExists = await User.findOne({ email: req.body.email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("email already exists");
+  }
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
