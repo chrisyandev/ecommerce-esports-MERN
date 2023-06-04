d471f9d620e78bed2708305a08373115e28e0fb6
chrisyandev <czyanhome@hotmail.com>
2023-06-03 21:02:24 -0700

    refactor: add createTokenUser() util function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
-const { attachTokenToResponse } = require("../utils");
+const { attachTokenToResponse, createTokenUser } = require("../utils");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -21,12 +21,7 @@ const register = async (req, res) => {
     role: role,
   });
 
-  const tokenUser = {
-    name: user.name,
-    userId: user._id,
-    role: user.role,
-  };
-
+  const tokenUser = createTokenUser(user);
   attachTokenToResponse({ res: res, user: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
@@ -51,12 +46,7 @@ const login = async (req, res) => {
     throw new CustomError.UnauthenticatedError("invalid password");
   }
 
-  const tokenUser = {
-    name: userFound.name,
-    userId: userFound._id,
-    role: userFound.role,
-  };
-
+  const tokenUser = createTokenUser(user);
   attachTokenToResponse({ res: res, user: tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
--- server/utils/index.js
+++ server/utils/index.js
@@ -1,7 +1,9 @@
 const { createJWT, isTokenValid, attachTokenToResponse } = require("./jwt");
+const createTokenUser = require("./createTokenUser");
 
 module.exports = {
   createJWT,
   isTokenValid,
   attachTokenToResponse,
+  createTokenUser,
 };
