eac534ccc5fea296b0dd57a303e856bb89746af2
chrisyandev <czyanhome@hotmail.com>
2023-06-04 22:10:42 -0700

    add product controller and routes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 // express
 const app = express();
@@ -31,6 +32,7 @@ app.get("/api/v1", (req, res) => {
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 // error middleware
 app.use(noRouteHandler);
