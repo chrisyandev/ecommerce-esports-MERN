3fb363ab0f57b53e5b4a0e6dbb300f475285c1c9
chrisyandev <czyanhome@hotmail.com>
2023-06-05 19:37:00 -0700

    work on createProduct()

--- server/models/Product.js
+++ server/models/Product.js
@@ -39,6 +39,7 @@ const ProductSchema = new mongoose.Schema(
     },
     colors: {
       type: [String],
+      default: undefined,
       required: [true, "please provide product colors"],
     },
     featured: {
@@ -57,7 +58,7 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    user: {
+    userId: {
       type: mongoose.Types.ObjectId,
       ref: "User",
       required: true,
