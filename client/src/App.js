d5fecc2118d45f5622c550640f0ae3e673a74c2c
chrisyandev <czyanhome@hotmail.com>
2023-06-11 16:40:43 -0700

    add some React routes pages and components, remove testing files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
+import { Footer, Navbar, Sidebar } from "./components";
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="products/:id" element={<SingleProductPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
