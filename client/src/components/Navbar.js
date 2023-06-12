0d8e9812cf312aea6304e5a74a71d65054191aed
chrisyandev <czyanhome@hotmail.com>
2023-06-11 18:53:37 -0700

    work on Navbar and UserToolbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,104 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links } from "../utils/constants";
+import UserToolbar from "./UserToolbar";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <StyledNav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Esports Shop" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <UserToolbar />
+      </div>
+    </StyledNav>
+  );
 };
+
+const StyledNav = styled.nav`
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .nav-center {
+    width: 90vw;
+    margin: 0 auto;
+    max-width: var(--max-width);
+  }
+  .nav-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      width: 175px;
+      margin-left: -15px;
+    }
+  }
+  .nav-toggle {
+    background: transparent;
+    border: transparent;
+    color: var(--clr-primary-5);
+    cursor: pointer;
+    svg {
+      font-size: 2rem;
+    }
+  }
+  .nav-links {
+    display: none;
+  }
+  .user-toolbar-wrapper {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .nav-toggle {
+      display: none;
+    }
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr auto;
+      align-items: center;
+    }
+    .nav-links {
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 0.5rem;
+      }
+      a {
+        color: var(--clr-grey-3);
+        font-size: 1rem;
+        text-transform: capitalize;
+        letter-spacing: var(--spacing);
+        padding: 0.5rem;
+        &:hover {
+          border-bottom: 2px solid var(--clr-primary-7);
+        }
+      }
+    }
+    .user-toolbar-wrapper {
+      display: grid;
+    }
+  }
+`;
 
 export default Navbar;
