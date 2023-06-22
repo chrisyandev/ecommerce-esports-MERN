0f7b0da1a2d4b25ee6b9f6b9a1fb21fa37c3dd94
chrisyandev <czyanhome@hotmail.com>
2023-06-21 18:52:54 -0700

    add sorting functionality

--- client/src/contexts/filter-context.js
+++ client/src/contexts/filter-context.js
@@ -3,7 +3,11 @@ import { useProductsContext } from "./products-context";
 import filterReducer from "../reducers/filter-reducer";
 import {
   PRODUCTS_LOAD,
+  PRODUCTS_SORT,
+  PRODUCTS_FILTER,
   PRODUCT_SORT_TYPE_UPDATE,
+  PRODUCT_FILTERS_UPDATE,
+  PRODUCT_FILTERS_CLEAR,
 } from "../actions/filter-actions";
 import { productSortTypes } from "../utils/constants";
 
@@ -22,6 +26,10 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: PRODUCTS_LOAD, payload: products });
   }, [products]);
+
+  useEffect(() => {
+    dispatch({ type: PRODUCTS_SORT });
+  }, [products, state.productSortType]);
 
   const updateProductSortType = (e) => {
     const value = e.target.value;
--- client/src/reducers/filter-reducer.js
+++ client/src/reducers/filter-reducer.js
@@ -1,7 +1,9 @@
 import {
   PRODUCTS_LOAD,
+  PRODUCTS_SORT,
   PRODUCT_SORT_TYPE_UPDATE,
 } from "../actions/filter-actions";
+import { productSortTypes } from "../utils/constants";
 
 const filterReducer = (state, action) => {
   if (action.type === PRODUCTS_LOAD) {
@@ -14,6 +16,25 @@ const filterReducer = (state, action) => {
 
   if (action.type === PRODUCT_SORT_TYPE_UPDATE) {
     return { ...state, productSortType: action.payload };
+  }
+
+  if (action.type === PRODUCTS_SORT) {
+    const { productSortType, filteredProducts } = state;
+
+    if (productSortType === productSortTypes.PRICE_LOW_TO_HIGH) {
+      filteredProducts.sort((a, b) => a.price - b.price);
+    }
+    if (productSortType === productSortTypes.PRICE_HIGH_TO_LOW) {
+      filteredProducts.sort((a, b) => b.price - a.price);
+    }
+    if (productSortType === productSortTypes.NAME_A_TO_Z) {
+      filteredProducts.sort((a, b) => a.name.localeCompare(b.name));
+    }
+    if (productSortType === productSortTypes.NAME_Z_TO_A) {
+      filteredProducts.sort((a, b) => b.name.localeCompare(a.name));
+    }
+
+    return { ...state };
   }
 
   throw new Error(`no matching action type: ${action.type}`);
