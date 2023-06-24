cc4fdf011f6fe9e07d2dc3d5f57083ef92a87c9d
chrisyandev <czyanhome@hotmail.com>
2023-06-24 16:24:04 -0700

    set up CartColumns and CartTotals

--- client/src/components/Cart/CartColumns.js
+++ client/src/components/Cart/CartColumns.js
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
 const CartColumns = () => {
-  return <h4>Cart Columns</h4>;
+  return (
+    <StyledDiv>
+      <div className="content">
+        <h5>Item</h5>
+        <h5>Price</h5>
+        <h5>Quantity</h5>
+        <h5>Subtotal</h5>
+        <span />
+      </div>
+      <hr />
+    </StyledDiv>
+  );
 };
+
+const StyledDiv = styled.div`
+  display: none;
+  @media (min-width: 776px) {
+    display: block;
+    .content {
+      display: grid;
+      grid-template-columns: 316px 1fr 1fr 1fr auto;
+      justify-items: center;
+      column-gap: 1rem;
+      h5 {
+        color: var(--clr-grey-5);
+        font-weight: 400;
+      }
+    }
+
+    span {
+      width: 2rem;
+      height: 2rem;
+    }
+    hr {
+      margin-top: 1rem;
+      margin-bottom: 3rem;
+    }
+  }
+`;
 
 export default CartColumns;
--- client/src/components/Cart/CartTotals.js
+++ client/src/components/Cart/CartTotals.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { useCartContext } from "../../contexts/cart-context";
+import { formatPrice } from "../../utils/helpers";
 
 const CartTotals = () => {
-  return <h4>Cart Totals</h4>;
+  const { totalAmount, shippingFee } = useCartContext();
+
+  return (
+    <StyledSection>
+      <div>
+        <article>
+          <h5>
+            Subtotal : <span>{formatPrice(totalAmount)}</span>
+          </h5>
+          <p>
+            Shipping Fee : <span>{formatPrice(shippingFee)}</span>
+          </p>
+          <hr />
+          <h4>
+            Order Total : <span>{formatPrice(totalAmount + shippingFee)}</span>
+          </h4>
+        </article>
+        <Link to="/checkout" className="btn">
+          Checkout
+        </Link>
+      </div>
+    </StyledSection>
+  );
 };
+
+const StyledSection = styled.section`
+  margin-top: 3rem;
+  display: flex;
+  justify-content: center;
+  article {
+    border: 1px solid var(--clr-grey-8);
+    border-radius: var(--radius);
+    padding: 1.5rem 3rem;
+  }
+  h4,
+  h5,
+  p {
+    display: grid;
+    grid-template-columns: 200px 1fr;
+  }
+  p {
+    text-transform: capitalize;
+  }
+  h4 {
+    margin-top: 2rem;
+  }
+  @media (min-width: 776px) {
+    justify-content: flex-end;
+  }
+  .btn {
+    width: 100%;
+    margin-top: 1rem;
+    text-align: center;
+    font-weight: 700;
+  }
+`;
 
 export default CartTotals;
