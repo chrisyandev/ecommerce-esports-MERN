02cf1c4755cb30a05536dabed993bdd76126f70e
chrisyandev <czyanhome@hotmail.com>
2023-06-29 13:41:21 -0700

    add CSS provided by Stripe, add Stripe directives to Content Security Policy

--- server/server.js
+++ server/server.js
@@ -31,7 +31,16 @@ const port = process.env.PORT || 5000;
 app.use(mongoSanitizer());
 app.use(rateLimiter({ windowMs: 600000, max: 50 }));
 app.use(xssCleaner());
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      connectSrc: ["'self'", "https://checkout.stripe.com"],
+      frameSrc: ["'self'", "https://checkout.stripe.com"],
+      scriptSrc: ["'self'", "https://checkout.stripe.com"],
+      imgSrc: ["'self'", "https://*.stripe.com"],
+    },
+  })
+);
 app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
